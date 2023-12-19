89cf5a10a63f84f424330fde85fd5ca2c2ed2f2c
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-19 18:50:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const denverBroncosRecord = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+const superbowlWin = (record) => {
+  const winningGame = record.find((game) => game.result === "W");
+  //   return winningGame ? winningGame.year : undefined;
+  if (winningGame && winningGame.result === "W") {
+    return winningGame.year;
+  } else {
+    return undefined;
+  }
+};
+
+const winYear = superbowlWin(denverBroncosRecord);
+console.log(winYear);
